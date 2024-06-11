edcd5bd71534ae812d45563383aa733be2c468c3
Marcao e Ludi <marcaoeludi@MacBook-Air-de-Marcao.local>
2024-06-10 23:58:36 -0300

    Create upsert_delta

--- main.py
+++ main.py
@@ -1,62 +1,71 @@
 import polars as pl
 from datetime import datetime
-from deltalake import DeltaTable
-
-df = pl.DataFrame(
-    {
-        "id": [1, 2, 3],
-        "data_aniversario": [
-            datetime(2025, 1, 1),
-            datetime(1993, 9, 19),
-            datetime(1960, 5, 31),
-        ],
-        "nota": [4.0, 5.0, 6.0],
-        "pessoa": ["ludmila", "joao", "deise"],
-    }
-)
-
-pl.Config.set_tbl_rows(100)
-
-df.write_parquet("data/output.parquet")
-
-df = pl.read_delta('data/data_parquet/')
-
-print(df)
+import os
 
 
-table_path = "data/data_parquet/"
-df.write_delta(table_path)
-
-df_new = pl.DataFrame(
-    {
-        "id": [1],
-        "data_aniversario": [
-            datetime(1993, 8, 19),
-        ],
-        "nota": [4.2],
-        "pessoa": ["igor"]
-    }
-)
+def is_directory_empty(directory):
+    # Lista os arquivos no diretório
+    files = os.listdir(directory)
+    # Verifica se a lista está vazia
+    print(len(files) == 0)
+    return len(files) == 0
 
 
-primary_keys = ['id', 'pessoa']
+def upsert_delta(primary_keys, data_path, df):
 
-string_exp = ""
+    if is_directory_empty(data_path):
+        df.write_delta(data_path)
+    else:
 
-for pk in primary_keys:
-    string_exp += f"s.{pk} = t.{pk}" if string_exp == "" else f" AND s.{pk} = t.{pk}"
+        string_exp = ""
 
-(
-    df_new.write_delta(
-        "data/data_parquet/",
-        mode="merge",
-        delta_merge_options={
-            "predicate": string_exp,
-            "source_alias": "s",
-            "target_alias": "t",
-        },
+        for pk in primary_keys:
+            string_exp += (
+                f"s.{pk} = t.{pk}" if string_exp == "" else f" AND s.{pk} = t.{pk}"
+            )
+
+        (
+            df.write_delta(
+                data_path,
+                mode="merge",
+                delta_merge_options={
+                    "predicate": string_exp,
+                    "source_alias": "s",
+                    "target_alias": "t",
+                },
+            )
+            .when_matched_update_all()
+            .when_not_matched_insert_all()
+            .execute()
+        )
+
+
+def main():
+
+    pl.Config.set_tbl_rows(100)
+
+    df = pl.read_delta("data/data_parquet/")
+
+    df_new = pl.DataFrame(
+        {
+            "id": [1, 2, 3],
+            "data_aniversario": [
+                datetime(2025, 1, 1),
+                datetime(1993, 9, 19),
+                datetime(1960, 5, 31),
+            ],
+            "nota": [4.0, 5.0, 6.0],
+            "pessoa": ["ludmila", "joao", "deise"],
+        }
     )
-    .when_matched_update_all()
-    .when_not_matched_insert_all()
-    .execute()
-)
+
+    pk = ["id", "pessoa"]
+
+    upsert_delta(primary_keys=pk, data_path="data/data_parquet", df=df_new)
+
+    df = pl.read_delta("data/data_parquet/")
+
+    print(df)
+
+if __name__ == "__main__":
+    main()
